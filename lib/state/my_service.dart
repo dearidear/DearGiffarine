93294ea8bcc74083d07f76e7a973ca2873462c38
dearGiffarine <wanaporn.kl@gmail.com>
2020-11-24 11:59:14 +0700

    Update 24Nov

--- lib/state/my_service.dart
+++ lib/state/my_service.dart
@@ -1,3 +1,7 @@
+import 'dart:ui';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:deargiffarine/models/user_model.dart';
 import 'package:deargiffarine/state/authen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -9,6 +13,34 @@ class MyService extends StatefulWidget {
 }
 
 class _MyServiceState extends State<MyService> {
+  UserModel userModel;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    readData();
+  }
+
+  Future<Null> readData() async {
+    await Firebase.initializeApp().then((value) async {
+      await FirebaseAuth.instance.authStateChanges().listen((event) async {
+        String uid = event.uid;
+        print('uid ==>> $uid');
+        await FirebaseFirestore.instance
+            .collection('user')
+            .doc(uid)
+            .snapshots()
+            .listen((event) {
+          setState(() {
+            userModel = UserModel.fromMap(event.data());
+            print('name ==>> ${userModel.name}, email ==>> ${userModel.email}');
+          });
+        });
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,40 +50,118 @@ class _MyServiceState extends State<MyService> {
   }
 
   Drawer buildDrawer() => Drawer(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
+        child: Stack(
           children: [
-            Container(
-              decoration: BoxDecoration(color: Colors.teal),
-              child: ListTile(
-                onTap: () async {
-                  await Firebase.initializeApp().then((value) async {
-                    await FirebaseAuth.instance
-                        .signOut()
-                        .then((value) => Navigator.pushAndRemoveUntil(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => Authen(),
-                            ),
-                            (route) => false));
-                  });
-                },
-                leading: Icon(
-                  Icons.exit_to_app,
-                  color: Colors.white,
-                  size: 36,
+            buildSignOut(),
+            Column(
+              children: [
+                buildUserAccountsDrawerHeader(),
+                buildListTileListPost(),
+                Divider(
+                  color: Colors.teal.shade400,
                 ),
-                title: Text(
-                  'Sing Out',
-                  style: TextStyle(color: Colors.white),
+                buildListTileInformation(),
+                Divider(
+                  color: Colors.teal.shade400,
                 ),
-                subtitle: Text(
-                  'ออกจาก Account เพื่อ Log In ใหม่',
-                  style: TextStyle(color: Colors.white54),
-                ),
-              ),
+              ],
             ),
           ],
         ),
       );
+
+  ListTile buildListTileListPost() {
+    return ListTile(
+      leading: Icon(
+        Icons.assignment,
+        size: 36,
+        color: Colors.teal,
+      ),
+      title: Text('Show List Post'),
+      subtitle: Text('แสดง Post ทั้งหมดที่มีในฐานข้อมูล'),
+      onTap: () {
+        Navigator.pop(context);
+      },
+    );
+  }
+
+  ListTile buildListTileInformation() {
+    return ListTile(
+      leading: Icon(
+        Icons.account_box,
+        size: 36,
+        color: Colors.teal,
+      ),
+      title: Text('Information'),
+      subtitle: Text('Display Information of User Logined'),
+      onTap: () {
+        Navigator.pop(context);
+      },
+    );
+  }
+
+  UserAccountsDrawerHeader buildUserAccountsDrawerHeader() {
+    return UserAccountsDrawerHeader(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('images/wall.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      currentAccountPicture: Image.asset('images/logo1.png'),
+      accountName: Text(
+        userModel == null ? 'Name' : userModel.name,
+        style: TextStyle(
+          color: Colors.teal.shade600,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      accountEmail: Text(
+        userModel == null ? 'Email' : userModel.email,
+        style: TextStyle(
+          color: Colors.teal.shade600,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Widget buildSignOut() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Container(
+          decoration: BoxDecoration(color: Colors.teal),
+          child: ListTile(
+            onTap: () async {
+              await Firebase.initializeApp().then((value) async {
+                await FirebaseAuth.instance
+                    .signOut()
+                    .then((value) => Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Authen(),
+                        ),
+                        (route) => false));
+              });
+            },
+            leading: Icon(
+              Icons.exit_to_app,
+              color: Colors.white,
+              size: 36,
+            ),
+            title: Text(
+              'Sign Out',
+              style: TextStyle(color: Colors.white),
+            ),
+            subtitle: Text(
+              'ออกจาก Account เพื่อ Log In ใหม่',
+              style: TextStyle(color: Colors.white54),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
---
+++ lib/state/show_list_post.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class ShowListPost extends StatefulWidget {
+  @override
+  _ShowListPostState createState() => _ShowListPostState();
+}
+
+class _ShowListPostState extends State<ShowListPost> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('This is Show List Post'),
+    );
+  }
+}
